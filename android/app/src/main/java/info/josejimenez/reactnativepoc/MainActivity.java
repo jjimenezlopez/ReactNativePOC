91dabbe2f4665ae6ad50775e64d817e82cd7a9cc
Jose Jimenez <jjimenezlopez@gmail.com>
2017-11-07 11:10:04 +0100

    Fixing FB authentication in Android. Adding image to splash screen in Android.

--- android/app/src/main/java/info/josejimenez/reactnativepoc/MainActivity.java
+++ android/app/src/main/java/info/josejimenez/reactnativepoc/MainActivity.java
@@ -1,8 +1,39 @@
 package info.josejimenez.reactnativepoc;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        ImageView imageView = new ImageView(this);
+
+        view.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        view.setGravity(Gravity.CENTER);
+
+        // hard code the width and the height of the logo
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(400, 400);
+        layoutParams.gravity = Gravity.CENTER;
+        imageView.setLayoutParams(layoutParams);
+        imageView.setImageDrawable(ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.app_logo));
+
+        view.addView(imageView);
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
 }
--- android/app/src/main/java/info/josejimenez/reactnativepoc/MainApplication.java
+++ android/app/src/main/java/info/josejimenez/reactnativepoc/MainApplication.java
@@ -1,9 +1,12 @@
 package info.josejimenez.reactnativepoc;
 
 import android.app.Application;
+import android.content.Intent;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
+import com.reactnativenavigation.controllers.ActivityCallbacks;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -12,12 +15,40 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 import com.reactnativenavigation.NavigationApplication;
+
+import com.facebook.FacebookSdk;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    setActivityCallbacks(new ActivityCallbacks() {
+      @Override
+      public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+      }
+    });
+
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
+  }
 
   @Override
   public boolean isDebug() {
@@ -26,13 +57,15 @@ public class MainApplication extends NavigationApplication {
   }
 
   protected List<ReactPackage> getPackages() {
+    
     return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new FIRMessagingPackage(),
-          new RNSoundPackage(),
-          new VectorIconsPackage(),
-          new RNFetchBlobPackage(),
-          new ReactNativeAudioPackage()
+        new FBSDKPackage(mCallbackManager),
+        new FIRMessagingPackage(),
+        new RNSoundPackage(),
+        new VectorIconsPackage(),
+        new RNFetchBlobPackage(),
+        new ReactNativeAudioPackage()
     );
   }
 
